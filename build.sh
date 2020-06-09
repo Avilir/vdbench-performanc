b4d55d9e4d47f8315a740d5265a8c7463ce41cff
Avi liani <alayani@redhat.com>
2020-06-09 18:58:30 +0300

    Adding remarks in the script - build.sh
    Signed-off-by: Avi liani <alayani@redhat.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ##############################################################################
 #
@@ -17,10 +17,10 @@
 
 Tag=$1
 
-#Repo="quay.io/avili/vdbench"
 # This is the repository that the container will push into.
 # This need to be change if you want to use a different repository.
-Repo="docker-registry.upshift.redhat.com/vdbench/vdbench"
+#Repo="docker-registry.upshift.redhat.com/vdbench/vdbench"
+Repo="quay.io/avili/vdbench"
 
 CMD_TOOL='podman'  # if you want this can be replace with : 'docker'
 
@@ -28,7 +28,7 @@ zip_file="vdbench50407.zip"
 
 if [[ ! -f ${zip_file} ]] ; then
   echo "The source file from Oracle (${zip_file}) is not present."
-  echo "Pleas download it and reun again."
+  echo "Pleas download it and rerun again."
   exit 1
 fi
 
@@ -37,28 +37,39 @@ if [[ ${Tag} == "" ]] ; then
 	exit 1
 fi
 
+echo "Creating Bin Directory : bin"
 mkdir bin
 if [[ $? -ne 0 ]] ; then
   echo "can not create directory !"
   exit 1
 fi
 
+echo "Moving the zip file (${zip_file}) to bin directory"
 mv ${zip_file} bin/
 cd bin
+
+echo "Unzip the zip file"
 unzip -q ${zip_file}
+
+echo "Delete unnecessary files"
 rm -rf aix windows hp mac sol* example* *.txt *.bat *.pdf
 rm -rf linux/config.sh linux/linux32.so linux/sparc64.so
 
+echo "Return zip file to parent directory"
 mv vdbench50407.zip ../
 cd classes/Vdb
+
+echo "Downloading needed class file"
 curl -Os https://community.oracle.com/servlet/JiveServlet/downloadBody/1025084-102-1-177296/CollectSlaveStats.class
 if [[ $? -ne 0 ]] ; then
-  echo "Can not retrive class file"
+  echo "Can not retrieve class file"
   exit 1
 fi
 
 cd ../../../
 
+echo "Build the container image"
 ${CMD_TOOL} build -t ${Repo}:${Tag} .
 
+echo "Cleanup....."
 rm -rf bin
